240ad5c3b7ca6ef01c9017d872a586c1ae216284
j <himynameisjoeq@gmail.com>
2018-10-20 01:03:46 -0600

    #03 form init

--- TrieGame/Form1.cs
+++ TrieGame/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TrieGame
 {
     public partial class Form1 : Form
     {
+        Trie _trie;
+        List<string> _result;
+
         public Form1()
         {
             InitializeComponent();
+            _trie = new Trie();
+            _result = new List<string>();
+            _trie.Build(File.ReadAllLines("wordsEn.txt"));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
