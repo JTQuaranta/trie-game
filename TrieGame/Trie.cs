e41b5cafa8a7fc2dcc9e730f771cfd55331d590e
j <himynameisjoeq@gmail.com>
2018-10-19 23:58:19 -0600

    #01 trie half finished

---
+++ TrieGame/Trie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieGame
+{
+    class Trie
+    {
+        private TrieNode _root;
+
+        public Trie()
+        {
+            _root = new TrieNode(' ');
+        }
+
+        public void Build(string[] input)
+        {
+            foreach (string n in input)
+            {
+                this.addWord(n);
+            }
+        }
+
+        public void addWord(string word)
+        {
+            TrieNode child = null;
+            TrieNode current = _root;
+
+
+        }
+
+        public List<string> getList(string word)
+        {
+
+        }
+
+        private List<string> getLists(TrieNode node, string prefix)
+        {
+
+        }
+
+    }
+}
---
+++ TrieGame/TrieNode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieGame
+{
+    class TrieNode
+    {
+        private char _value;
+        private List<TrieNode> _subs;
+
+        #region Constructors
+
+        public TrieNode(char c)
+        {
+            _value = c;
+            _subs = new List<TrieNode>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected void addSub(TrieNode sub)
+        {
+            this._subs.Add(sub);
+        }
+
+        public TrieNode getChild(char c)
+        {
+            foreach (TrieNode item in this._subs)
+            {
+                if (item.Value == c)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public char Value
+        {
+            get { return _value; }
+            private set { _value = value; }
+        }
+
+        public List<TrieNode> Subs
+        {
+            get { return _subs; }
+        }
+
+        #endregion
+    }
+}
